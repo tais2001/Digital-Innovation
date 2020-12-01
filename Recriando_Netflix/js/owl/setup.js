aef3d82fdc12bc5b77840f2447ad40ac003990a7
Tais Itoo <tais2001a@gmail.com>
2020-12-01 17:47:21 -0300

    Recriando o Netflix

---
+++ Recriando_Netflix/js/owl/setup.js
@@ -0,0 +1,28 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function clicou() {
+    window.open("https://www.youtube.com/watch?v=kasvV-XyzqU");
+}
+
+function info() {
+    window.open("https://pt.wikipedia.org/wiki/House_of_Cards_(s%C3%A9rie_americana)");
+}
+
+function inicio() {
+    window.open("https://www.netflix.com/br/");
+}
