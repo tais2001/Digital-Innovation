66d08331153feaa54cc073f3848df8eb7b1d686b
Tais Itoo <tais2001a@gmail.com>
2020-12-02 23:18:58 -0300

    criar comidinha

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ let box = 32;
 let snake = [];
 snake[0] = {
   x: 8 * box,
-  y: 8 * box,
-};
+  y: 8 * box
+}
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
   context.fillStyle = "lightgreen";
@@ -14,30 +18,36 @@ function criarBG() {
 }
 
 function criarCobrinha() {
-  for (i = 0; i < snake.length; i++) {
+  for (i=0; i < snake.length; i++) {
     context.fillStyle = "green";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
 }
 
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener("keydown", update);
 
 function update(event) {
-  if (event.keyCode == 37 && direction != "right") direction = "left";
-  if (event.keyCode == 38 && direction != "down") direction = "up";
-  if (event.keyCode == 39 && direction != "left") direction = "right";
-  if (event.keyCode == 40 && direction != "up") direction = "down";
+  if(event.keyCode == 37 && direction != "right") direction = "left";
+  if(event.keyCode == 38 && direction != "down") direction = "up";
+  if(event.keyCode == 39 && direction != "left") direction = "right";
+  if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
 function iniciarJogo() {
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 *box;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
 
   criarBG();
   criarCobrinha();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
@@ -52,7 +62,7 @@ function iniciarJogo() {
   let newHead = {
     x: snakeX,
     y: snakeY,
-  };
+  }
 
   snake.unshift(newHead);
 }
